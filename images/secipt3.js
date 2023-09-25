7401813c2b51b98704beb50c9f55a38bcba398da
chandhra003 <chandhrasekhar77803@gmail.com>
2023-09-25 16:31:55 +0530

    first commit

---
+++ images/secipt3.js
@@ -0,0 +1,37 @@
+function demo(name){
+    console.log(name)
+}
+demo('abhi')
+demo('vaibhav')
+
+// Additioon two numbers
+function sum(num1,num2){
+    result=num1+num2
+    document.write(result);
+}
+sum(3,8)
+
+var add=function sum(num1,num2){
+    result=num1+num2
+    document.write(result);
+}
+add(7,8)
+
+var a=Math.ceil(8.5)
+console.log(a)
+var a=Math.ceil(-8.5)
+console.log(a)
+
+var a=Math.floor(-9.5)
+console.log(a)
+var a=Math.floor(9.5)
+console.log(a)
+
+var b=Math.sqrt(169)
+console.log(b)
+
+var c=Math.random()
+console.log(c)
+
+var d=Math.round(100*10)/33
+console.log(d)
